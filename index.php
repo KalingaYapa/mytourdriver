beec3c4aba4489fef0f08112d474029161a0df58
KalingaYapa <kkyapa@gmail.com>
2016-10-31 14:11:51 +0530

    Login Modal

--- index.php
+++ index.php
@@ -136,6 +136,8 @@
                                 </li>
                                 <li><a class="tagline" style="border-radius:20px; cursor: pointer; border:1px solid #FFF; padding: 2px 5px; color: #FFF; font-weight: 500;"  data-toggle="modal" data-target="#contactModal"><i class="fa fa-info-circle"></i> kkyapa@gmail.com</a>
                                 </li>
+                                <li><a class="tagline" style="border-radius:20px; cursor: pointer; border:1px solid #FFF; padding: 2px 5px; color: #FFF; font-weight: 500;"  data-toggle="modal" data-target="#loginModal"><i class="fa fa-info-circle"></i> Login</a>
+                                </li>
                             </ul>
                         </div>
                         <div class="menu-wrap">
@@ -395,8 +397,7 @@
                     </div>
 
                     <div class="modal-footer">
-                        <!--        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>-->
+
                         <div id="sending_message"></div>
                         <div id="success_message"></div>
 
@@ -406,6 +407,48 @@
         </div>
 
         <!--end contact model-->
+
+        <!--Start Login Modal -->
+        <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Login</h4>
+                    </div>
+                    <div class="modal-body">
+
+                        <form class="form-horizontal" role="form" id="login_form">
+
+                            <div class="form-group">
+                                <label class="control-label col-sm-3" for="pwd">UserName:</label>
+                                <div class="col-sm-9"> 
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-sm-3" for="email">Password:</label>
+                                <div class="col-sm-9">
+                                    <input type="password" class="form-control" id="email" name="email" placeholder="">
+                                </div>
+                            </div>
+                            <div class=" col-sm-offset-8 "> 
+                                <input type="submit" class="btn btn-danger" id="send_login_submit" value="Login">
+                            </div> 
+                        </form>
+                    </div>
+
+                    <div class="modal-footer">
+
+                        <div id="sending_message"></div>
+                        <div id="success_message"></div>
+
+                    </div> 
+                </div>
+            </div>
+        </div>
+
+        <!--End Login Modal -->
         <!--start Cooperate rental model-->
 
         <div class="modal fade" id="cooperatemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
